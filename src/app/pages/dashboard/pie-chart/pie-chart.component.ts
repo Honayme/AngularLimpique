8800b4d8d59e34dc1675c904e122d3ed044c7bd0
Thomas <thomas.mirabile@ynov.com>
2024-07-07 16:35:29 +0200

    Update node modules, fix angular config for canvajs, make the pie-chart component cleaner

--- src/app/pages/dashboard/pie-chart/pie-chart.component.ts
+++ src/app/pages/dashboard/pie-chart/pie-chart.component.ts
@@ -1,27 +1,29 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
-import {MedalsService} from "../../../core/services/medals.service";
-import {Subscription} from "rxjs";
-
+import { MedalsService } from '../../../core/services/medals.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CanvasJSAngularChartsModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CanvasJSAngularChartsModule
+  ],
   templateUrl: './pie-chart.component.html',
-  styleUrl: './pie-chart.component.scss'
+  styleUrls: ['./pie-chart.component.scss'] // Corrected typo from styleUrl to styleUrls
 })
-
-export class PieChartComponent implements OnInit, OnDestroy{
+export class PieChartComponent implements OnInit, OnDestroy {
   chartOptions: any;
   private subscription: Subscription | undefined;
 
-  constructor(private participationService: MedalsService) {}
+  constructor(private medalsService: MedalsService) {}
 
   ngOnInit(): void {
-    this.subscription = this.participationService.getParticipationData().subscribe(dataPoints => {
+    this.subscription = this.medalsService.getParticipationData().subscribe(dataPoints => {
       console.log(dataPoints);
       this.chartOptions = {
         animationEnabled: true,
@@ -31,7 +33,7 @@ export class PieChartComponent implements OnInit, OnDestroy{
         data: [{
           type: "pie",
           startAngle: -90,
-          indexLabel: " {name}",
+          indexLabel: "{name}",
           yValueFormatString: "#,###.##'%'",
           dataPoints: dataPoints
         }]
@@ -44,23 +46,4 @@ export class PieChartComponent implements OnInit, OnDestroy{
       this.subscription.unsubscribe();
     }
   }
-
-/*  chartOptions = {
-    animationEnabled: true,
-    title: {
-      text: "Sales by Department"
-    },
-    data: [{
-      type: "pie",
-      startAngle: -90,
-      indexLabel: "{name}: {y}",
-      yValueFormatString: "#,###.##'%'",
-      dataPoints: [
-        { y: 14.1, name: "Toys" },
-        { y: 28.2, name: "Electronics" },
-        { y: 14.4, name: "Groceries" },
-        { y: 43.3, name: "Furniture" }
-      ]
-    }]
-  }*/
 }
